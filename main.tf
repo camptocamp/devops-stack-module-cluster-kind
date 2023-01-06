cfaab3b902340d588cdc0990073e4c99301aad41
modridi <mohamed-amine.dridi@camptocamp.com>
2023-01-06 10:31:00 +0100

    refactor!: change module config

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-locals {
-  docker_gateway = compact(data.docker_network.kind_network.ipam_config[*].gateway)[0]
-  base_domain    = coalesce(var.base_domain, format("%s.nip.io", replace(local.docker_gateway, ".", "-")))
-  default_extra_port_mappings = [
-    "tcp/80",
-    "tcp/443",
-  ]
-}
-
-data "docker_network" "kind_network" {
-  # Name given to the Docker network by the kind_cluster resource so it cannot be anything other than `kind`.
-  name       = "kind"
-  depends_on = [kind_cluster.cluster]
-}
-
 resource "kind_cluster" "cluster" {
   name = var.cluster_name
 
@@ -24,21 +9,18 @@ resource "kind_cluster" "cluster" {
 
     node {
       role = "control-plane"
-
-      # Add annotation to specify that this node is where the ingress should be deployed.
-      kubeadm_config_patches = [
-        "kind: InitConfiguration\nnodeRegistration:\n  kubeletExtraArgs:\n    node-labels: \"ingress-ready=true\"\n"
-      ]
-
-      dynamic "extra_port_mappings" {
-        for_each = toset(concat(local.default_extra_port_mappings, var.extra_port_mappings))
-        content {
-          host_port      = element(split("/", extra_port_mappings.value), 1)
-          container_port = element(split("/", extra_port_mappings.value), 1)
-          protocol       = upper(element(split("/", extra_port_mappings.value), 0))
-        }
-      }
     }
-
+    # TODO variable for worker nodes with labels.
+    node {
+      role = "worker"
+    }
+    node {
+      role = "worker"
+    }
   }
+}
+
+data "docker_network" "kind" {
+  name       = "kind"
+  depends_on = [kind_cluster.cluster]
 }
--- variables.tf
+++ variables.tf
@@ -1,23 +1,9 @@
 variable "cluster_name" {
-  description = "The name to give to the cluster."
   type        = string
   default     = "kind"
 }
 
-variable "base_domain" {
-  description = "The base domain used for ingresses."
-  type        = string
-  default     = null
-}
-
 variable "kubernetes_version" {
-  description = "Kubernetes version to use for the KinD cluster (images available https://hub.docker.com/r/kindest/node/tags[here])."
   type        = string
-  default     = "v1.25.3" # Define here the latest version of Kubernetes.
-}
-
-variable "extra_port_mappings" {
-  description = "Map of extra ports to open on the control-plane node."
-  type        = list(string)
-  default     = ["tcp/80", "tcp/443"]
+  default     = "v1.25.3"
 }
