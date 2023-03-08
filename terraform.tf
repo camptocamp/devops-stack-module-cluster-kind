540fc10ea45b6eacab5bea42b165b6ec3f16ce11
Gonçalo Heleno <33546359+lentidas@users.noreply.github.com>
2023-03-08 17:27:56 +0100

    fix: change to looser versions constraints as per best practices (#11)
    * fix: change to looser versions constraints as per best practices
    
    See documentation [here](https://developer.hashicorp.com/terraform/language/providers/requirements#version-constraints).
    
    * docs(terraform-docs): generate docs and write to README.adoc
    
    ---------
    
    Co-authored-by: lentidas <lentidas@users.noreply.github.com>

--- terraform.tf
+++ terraform.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     kind = {
       source  = "tehcyx/kind"
-      version = "~> 0.0.16"
+      version = ">= 0.0.16"
     }
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 2.23.1"
+      version = ">= 2.23.1"
     }
   }
 }
